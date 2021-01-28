e2029118c3ce3cb653fd62efb342fde6d20379b4
simonacossai <63307173+simonacossai@users.noreply.github.com>
2021-01-28 09:31:02 +0100

    DONE- fixing some styling issue

--- src/components/ProfileDetailsCard.jsx
+++ src/components/ProfileDetailsCard.jsx
@@ -38,15 +38,15 @@ export default class ProfileDetailsCard extends Component {
     }
     render() {
         return (   
-                <Card className="profile-detail-card">
+                <Card className="profile-detail-card d-flex justify-content-center align-items-center">
                 <Card.Img variant="top" src={cover} className="profile-detail-image"/>
                 <img src={this.state.user.image} className="profile-detail-profile-pic mx-auto d-flex" />
                 <Card.Body className="pb-2">
                     <Card.Title>{this.state.user.username ?? "Username"}</Card.Title>
                     <Card.Subtitle className=" text-muted">{this.state.user.title ?? "Web Developer"}</Card.Subtitle>
-                    <hr></hr>
-                    <Card.Text className="d-flex justify-content-between small-text-profile-detail"><p>Who viewed your profile</p> <p className="little-blue-numbers-detail">151</p> </Card.Text>
-                    <Card.Text className="d-flex justify-content-between small-text-profile-detail"><p>Views for the video </p> <p className="little-blue-numbers-detail">426</p></Card.Text>
+                    <hr style={{width:"100%"}} className="px-0 mx-0"></hr>
+                    <Card.Text className="d-flex justify-content-between small-text-profile-detail"><p>Who viewed your profile</p> <p className="little-blue-numbers-detail ml-2">151</p> </Card.Text>
+                    <Card.Text className="d-flex justify-content-between small-text-profile-detail"><p>Views for the video </p> <p className="little-blue-numbers-detail ml-2">426</p></Card.Text>
                     <Card.Footer className="pt-2 p-0 text-left card-footer-text "><BsBookmarkFill className="mr-2"/>My items</Card.Footer>
                 </Card.Body>
             </Card>
